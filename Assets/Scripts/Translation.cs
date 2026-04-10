5ac5becca883fc55954e47d4a1ed330e9c3f927c
JulenMendez <j.mendez@opendeusto.es>
2026-04-10 19:40:53 +0200

    Translation finished

--- Assets/Scripts/Translation.cs
+++ Assets/Scripts/Translation.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class Translation : MonoBehaviour
 {
-    public Vector3[] ellipsePoints = new Vector3[0];
-    private int _ellipseSegments = 50;
-    private float _xScale = 149f;
-    private float _zScale = 150f;
+    
+    public int ellipseSegments = 50;
+    public float avgSunDistance;
+    public float e;
+    public float orbitalPeriod;
+    
+    private Vector3[] _ellipsePoints = new Vector3[0];
+    private float _xScale;
+    private float _zScale;
+    private float _orbitalPeriofSeconds;
+    private Vector3 _origin;
+    private Vector3 _destiny;
 
     int counter = 0;
 
     private void Awake()
     {
+        _zScale = avgSunDistance;
+        _xScale = avgSunDistance * Mathf.Sqrt(1 - Mathf.Pow(e, 2));
+
         CalculateEllipse();
     }
 
@@ -19,36 +30,72 @@ public class Translation : MonoBehaviour
     void Start()
     {
         this.GetComponent<MeshRenderer>().material.color = Color.blue;
-        for(int i = 0; i < _ellipseSegments; i++)
-        {
-            Debug.Log(ellipsePoints[i]);
-        }
+
+        _orbitalPeriofSeconds = (orbitalPeriod * 365 * 24 * 60 * 60)/1000000;
+        _origin = _ellipsePoints[counter];
+        _destiny = _ellipsePoints[counter + 1];
+
+        this.transform.position = _origin;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.position = ellipsePoints[counter];
-        counter++;
+        move(_origin, _destiny);
 
-        if (counter == _ellipseSegments)
+        if (this.transform.position == _destiny)
         {
-            counter = 0;
+            nextDestiny();
+        }
+
+
+        
+    }
+
+
+
+    void move(Vector3 origin, Vector3 destiny)
+    {
+        Vector3 direction = destiny - origin;
+        float velocity =  direction.magnitude / (_orbitalPeriofSeconds / ellipseSegments);
+        this.transform.position += direction.normalized * velocity * Time.deltaTime;
+
+        Vector3 myDirection = destiny - this.transform.position;
+        if (direction.normalized != myDirection.normalized)
+        {
+            this.transform.position = destiny;
         }
     }
 
+
+
+    void nextDestiny()
+    {
+        _origin = _destiny;
+        
+        counter++;
+
+        if (counter == ellipseSegments)
+        {
+            counter = 0;
+        }
+
+        _destiny = _ellipsePoints[counter];
+    }
+
+
     private void CalculateEllipse()
     {
-        ellipsePoints = new Vector3[_ellipseSegments];
+        _ellipsePoints = new Vector3[ellipseSegments];
 
-        for (int i = 0; i < _ellipseSegments; i++)
+        for (int i = 0; i < ellipseSegments; i++)
         {
-            float _angle = ((float)i / (float)_ellipseSegments) * 360 * Mathf.Deg2Rad;
+            float _angle = ((float)i / (float)ellipseSegments) * 360 * Mathf.Deg2Rad;
             float x = Mathf.Cos(_angle) * _xScale;
             float z = Mathf.Sin(_angle) * _zScale;
 
-            ellipsePoints[i] = new Vector3(x, 0f, z);
+            _ellipsePoints[i] = new Vector3(x, 0f, z);
         }
     }
 }

994bb1c02ed7bfc1a013dd6e21e9d59475eff73a
JulenMendez <j.mendez@opendeusto.es>
2026-04-16 06:06:06 +0200

    Added paused to the simulation

--- Assets/Scripts/TimeControler.cs
+++ Assets/Scripts/TimeControler.cs
@@ -4,9 +4,23 @@ public class TimeControler : MonoBehaviour
 {
     [Range(0,100)] public float newTimeScale;
 
+    private bool paused = false;
+
     // Update is called once per frame
     void Update()
     {
-        Time.timeScale = newTimeScale;
+        if (paused)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = newTimeScale;
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            paused = !paused;
+        }
     }
 }
